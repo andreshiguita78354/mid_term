ce2ba87754cf30033d1dd26e39dc159a10ca2c5a
andreshiguita78354 <36429197+andreshiguita78354@users.noreply.github.com>
2018-04-25 21:38:22 -0500

    Add files via upload

---
+++ andres_higuita.sql
@@ -0,0 +1,18 @@
+--------------------------------------------------------
+-- Archivo creado  - miércoles-abril-25-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Function FUNCTION_1
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "EJERCICIOS"."FUNCTION_1" (a int,b int,c int)
+return varchar2 is
+concat varchar(200);
+begin
+      if b>=c then
+        concat:=(to_char(a) || '/' || to_char(a*b) || '/' || 'andres');
+      else
+        concat:=(to_char(a) || '/' || to_char(a*b) || '/' || 'martinez');      
+      end if;
+      return concat;      
+end;
